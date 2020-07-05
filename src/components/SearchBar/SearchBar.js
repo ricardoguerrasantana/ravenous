e428036f4e8a85314cfd8d0e08df9040dadd9209
Ricarr <37304218+Repositorio-Ric@users.noreply.github.com>
2020-07-05 23:17:01 +1000

    Deploy a base structure of react components

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './SearchBar.css';
+
+const sortByOptions = {
+    "Best Match": 'best_match',
+    "Highest Rated": 'rating',
+    "Most Reviewed": 'review_count'
+}
+
+class SearchBar extends React.Component {
+    
+    renderSortByOptions() {
+        return Object.keys(sortByOptions).map(key => {
+            let sortByOptionValue = sortByOptions[key];
+            return <li key={sortByOptionValue}>{key}</li>;
+        });
+    }
+    
+    render() {
+        return (
+            <div className="SearchBar">
+            <div class="SearchBar-sort-options">
+                <ul>
+                    {this.renderSortByOptions()}
+                </ul>
+            </div>
+            <div className="SearchBar-fields">
+                <input placeholder="Search Businesses" />
+                <input placeholder="Where?" />
+            </div>
+            <div className="SearchBar-submit">
+                <a href="www.#.com">Let's Go</a>
+            </div>
+            </div>
+        );
+    }
+
+}
+
+export default SearchBar;
